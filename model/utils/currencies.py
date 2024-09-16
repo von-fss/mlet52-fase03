811a5afca26f1b910494e20a3090e698318c6a51
von-fss <felipe.ruiz.s@gmail.com>
2024-09-15 21:04:20 -0300

    Dataframe preparation. Final branch commit.

---
+++ model/utils/currencies.py
@@ -0,0 +1,19 @@
+
+currencies = {
+    'IRR': ['Iran'],
+    'XAF': ['Cameroon'],
+    'XOF': ['Benin'],
+    'JPY': ['Japan'],
+    'RUB': ['Russia'],
+    'INR': ['India'],
+    'CNY': ['China'],
+    'BRL': ['Brazil'],
+    'ILS': ['Israel'],
+    'XCD': ['Anguilla'],
+    'USD': ['United States'],
+    'AUD': ['Australia'],
+    'NZD': ['New Zealand'],
+    'EUR': ['France', 'Germany', 'Greece', 'Ireland', 'Italy', 'Portugal'],
+    'CHF': ['Switzerland'],
+    'KWD': ['Kuwait']
+}
